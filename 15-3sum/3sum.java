61f9c03797247666b0bf143d245b557362694c8f
Adnan Habib <98457152+ErAdnanHabib@users.noreply.github.com>
2024-08-03 10:51:46 +0530

    Time: 31 ms (59.12%) | Memory: 51.2 MB (84.54%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> triplets = new ArrayList<>();
-        Map<Integer, Integer> numToIndex = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            numToIndex.put(nums[i], i);
-        }
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                int target = -nums[i] - nums[j];
-                if (numToIndex.containsKey(target) && numToIndex.get(target) > j) {
-                    triplets.add(Arrays.asList(nums[i], nums[j], target));
+        List<List<Integer>> triplets= new ArrayList<>();
+        for(int i=0 ; i<=nums.length-3 ; i++){
+            if(i==0 || nums[i]!=nums[i-1]){
+                int left=i+1,right=nums.length-1;
+                int target =-nums[i];
+                while(left<right){
+                    if(nums[left]+nums[right]==target){
+                    triplets.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while(left < right && nums[left]==nums[left+1])left++;
+                    while(left < right && nums[right]==nums[right-1])right--;
+                    left++;
+                    right--;
+                    }else if(nums[left]+nums[right]<target){
+                        left++;
+                    }else{
+                        right--;
+                    }
                 }
             }
-        }
-
-        return triplets;
+        }return triplets;
+        
     }
 }
